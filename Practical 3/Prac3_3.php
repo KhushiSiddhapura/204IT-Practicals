04651861057650ddf62c78b32435698b1a67fd5d
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-08-16 13:10:56 +0530

    Practical 3 Done

--- Practical 3/Prac3_3.php
+++ Practical 3/Prac3_3.php
@@ -32,7 +32,7 @@
         </style>
     </head>
     <body>
-        <?php include "./Header.html"; ?>
+        <?php include "./Header.php"; ?>
         <main>
             <div
               class="
@@ -51,6 +51,6 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim beatae cupiditate tempora id nostrum, consectetur repellendus distinctio fugiat a nam dolore similique sit soluta ipsum earum laborum, doloribus vitae veniam? Sapiente magni consectetur praesentium dolorem molestiae facilis repudiandae laboriosam quasi vitae sunt. Officiis adipisci non earum animi sit ullam perspiciatis. Excepturi porro iusto debitis repellendus ipsam, impedit laboriosam accusantium sed a repellat sit odit dolorem eum modi dolores, corrupti dolore! Consectetur adipisci ipsum vero aut, iusto esse asperiores maxime mollitia labore delectus. Similique excepturi ipsum, beatae voluptate consequuntur enim tempora, deleniti atque minima fugit magni culpa accusantium mollitia dolores natus repellendus, est repudiandae. Blanditiis similique id atque numquam a ipsam exercitationem fugit, in incidunt quia commodi dignissimos molestias provident temporibus nostrum modi? Facilis repellendus dicta qui omnis voluptates, ipsa, itaque tenetur molestiae rem eligendi atque eveniet enim cum? Mollitia deleniti quod eos ipsa placeat, et eum cumque illum modi dignissimos nam a veniam quos odit enim nulla! Laborum optio quos quo, nemo consequatur, repudiandae est necessitatibus ipsum vitae nobis modi quibusdam qui quidem recusandae? Odio atque facere quo vel natus ipsam fugiat accusamus porro totam, quibusdam itaque dicta doloremque dolore aspernatur aut obcaecati cumque modi odit soluta qui? Quasi maxime labore iure, dolorum repellat eius enim. Eaque esse facere id impedit aut reiciendis laboriosam, ut delectus quos dolorem numquam non vel temporibus cupiditate, cum repudiandae, cumque eum provident molestias tenetur consequatur. Placeat aliquid fuga, ut minima numquam rerum in laborum odit architecto quae at alias, sint libero voluptatem nostrum mollitia?</p>
             </div>
         </main>
-        <?php include './Footer.html' ?>
+        <?php include './Footer.php' ?>
     </body>
 </html>
